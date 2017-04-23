bac3773afcbda2fb7ba2006a5776a3d0e81c3e40
Jim Stroming <jimstroming@gmail.com>
2017-04-23 10:37:31 -0500

    define the problem

---
+++ pickacard/pickacard.scala
@@ -0,0 +1,11 @@
+/* From a shuffled deck of 100 cards that are numbered 1 to 100, you are dealt 10
+  cards face down. You turn the cards over one by one. After each card, you must
+  decide whether to end the game. If you end the game on the highest card in the
+  hand you were dealt, you win; otherwise, you lose.
+
+What is the strategy that optimizes your chances of winning? 
+How does the strategy change as the sizes of the deck and the hand are changed? 
+
+*/
+
+
