05130e22c820e261f266c1c5249edc139f78a1df
Jim Stroming <jim.stroming@ibm.com>
2017-04-24 17:41:05 -0500

    The simple random number generator from the book Funtional Programming in Scala by Paul Chiusano and Runar Bjarnason

---
+++ pickacard/SimpleRNG.scala
@@ -0,0 +1,16 @@
+// Let's use the random number generator from Functional Programming in Scala 
+
+trait RNG {
+  def nextInt: (Int, RNG)
+}
+
+case class SimpleRNG(seed: Long) extends RNG {
+  def nextInt: (Int, RNG) = {
+    val newSeed = (seed * 0x5DEECE66DL + 0xBL) & 0xFFFFFFFFFFFFL
+    val nextRNG = SimpleRNG(newSeed)
+    val n = (newSeed >>> 16).toInt
+    (n, nextRNG)
+  }
+}
+
+
