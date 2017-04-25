3f3278459575263693bd51ecb86507181409aa1f
Jim Stroming <jim.stroming@ibm.com>
2017-04-25 17:30:57 -0500

    working version.  we should rename it

--- pickacard/HandofCardsGenerator.scala
+++ pickacard/HandofCardsGenerator.scala
@@ -4,27 +4,29 @@ class HandofCardsGenerator {
   //def nextHand: (Int, RNG) = {
   //  val rng = SimpleRNG(seed)
 
-  def getHand(handsize: Int, rng: RNG): (List[Int], RNG) = {
-    val (hand, simplerng) = buildHand(List(), rng, handsize)
+  def getHand(handsize: Int, decksize: Int, rng: RNG): (List[Int], RNG) = {
+    val (hand, simplerng) = buildHand(List(), rng, handsize, decksize)
     (hand, simplerng)
 
   }
     
 
-  def buildHand(hand: List[Int], rng: RNG, cardsleft: Int): (List[Int], RNG) = {
+  def buildHand(hand: List[Int], rng: RNG, cardsleft: Int, decksize: Int): (List[Int], RNG) = {
 
     if (cardsleft == 0) (hand, rng)
     else {
       
-      val (newcard, rngnext) = rng.nextInt  // generate a new card
+      val (newnumber, rngnext) = rng.nextInt  // generate a new number
+
+      val newcard =  ((newnumber.abs) % decksize) + 1         // convert it to a card from 1 to decksize
 
       if (hand.contains(newcard)) // check if it is already in the hand
 
-        buildHand(hand, rngnext, cardsleft) // if it is, call buildhand again
+        buildHand(hand, rngnext, cardsleft, decksize) // if it is, call buildhand again
 
       else // if it is not, add it to the hand and call buildhand again with one fewer card left 
 
-        buildHand(hand:::List(newcard), rngnext, cardsleft - 1)
+        buildHand(hand:::List(newcard), rngnext, cardsleft - 1, decksize)
     }
 
 
@@ -36,4 +38,12 @@ class HandofCardsGenerator {
 
 
 
-// val dealer = new HandofCardsGenerator
+/* 
+
+val dealer = new HandofCardsGenerator
+val rng = SimpleRNG(42)
+dealer.getHand(100, rng)
+
+
+*/
+
