4232fcb1ddc45a890c38b06264925e46cc439a8b
Jim Stroming <jim.stroming@ibm.com>
2017-04-28 07:47:19 -0500

    clean up

--- pickacard/CardSimulator.scala
+++ pickacard/CardSimulator.scala
@@ -24,37 +24,39 @@ How does the strategy change as the sizes of the deck and the hand are changed?
    
    The question then becomes, what do you do if the current card is the 
    highest card turned over so far. 
-*/
 
+   The problem is, we have incomplete information.   
+   The key is correctly identifying that the card you have is the max card.
+   It is very easy to screw up.  For example
+   (56, 30, 76,  40,  12, 60, 45, 58, 72, 14)
+   The max came early, in the third slot.  At that point, the odds
+   of the max being 76 was only 13%.   So we kept going and never
+   hit a new local max
+  
+   We can also miss the other way.  What about the following case
+   (56, 30, 70, 40, 12, 74, 29, 80, 12, 85)
+   Where do we stop?
+   When we drew 70 with card 3, that only had a 7% chance of being the high, so we kept going
+   On card 6, we drew 74.  This had a 27% chance of being high.  Probably keep going
+   On card 8, we drew 80.  This had a 61% chance of being high.  We would probably stop there.
+   But unfortunately, on card 10, we drew an 85. 
+  
+   What this shows is that, if there is a 50% chance of the current card being
+   the maximum, and you chose to draw another, your chance of winning becomes < 50%,
+   because even if the low card is in the face down cards, you may not identify it when it comes.
+  
+   Rule 2:  If the chance of your current card being max is 50% or more, stop.
+  
+   There is still room for improvement.  But I don't know how to quantify it right now.
+   Instead, I think we will need to simulate it, and try tuning that 50% lower to account for 
+   the fact we can not perfectly identify the max.
+
+*/
 
 object CardSimulator {
 
 
-  // The problem is, we have incomplete information.   
-  // The key is correctly identifying that the card you have is the max card.
-  // It is very easy to screw up.  For example
-  // (56, 30, 76,  40,  12, 60, 45, 58, 72, 14)
-  // The max came early, in the third slot.  At that point, the odds
-  // of the max being 76 was only 13%.   So we kept going and never
-  // hit a new local max
-  
-  // We can also miss the other way.  What about the following case
-  // (56, 30, 70, 40, 12, 74, 29, 80, 12, 85)
-  // Where do we stop?
-  // When we drew 70 with card 3, that only had a 7% chance of being the high, so we kept going
-  // On card 6, we drew 74.  This had a 27% chance of being high.  Probably keep going
-  // On card 8, we drew 80.  This had a 61% chance of being high.  We would probably stop there.
-  // But unfortunately, on card 10, we drew an 85. 
-  
-  // What this shows is that, if there is a 50% chance of the current card being
-  // the maximum, and you chose to draw another, your chance of winning becomes < 50%,
-  // because even if the low card is in the face down cards, you may not identify it when it comes.
-  
-  // Rule 2:  If the chance of your current card being max is 50% or more, stop.
-  
-  // There is still room for improvement.  But I don't know how to quantify it right now.
-  // Instead, I think we will need to simulate it, and try tuning that 50% lower to account for 
-  // the fact we can not perfectly identify the max.
+
   
   /* returns true if we stopped on the maximum card we were dealt */
 
