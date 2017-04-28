a2283c4d10caf67fac60d3fc3aa2eeb2821c7cf1
Jim Stroming <jim.stroming@ibm.com>
2017-04-28 17:07:41 -0500

    more reasoning

--- ballpainting/Simulator.scala
+++ ballpainting/Simulator.scala
@@ -40,7 +40,63 @@ import bagofballs.BagOfBalls
 
   The object, then, is to get from high card to four of a kind.
 
+  All states are not reachable from each other
 
+  from high card, you can only go to one pair
 
+  RBGY -> RRBG
 
+  from one pair, you can transition to three of a kind, two pair, or stay in one pair
+
+       -> RRRB
+  RRBG -> RRBB
+       -> RRBG
+
+  from two pair, you can go to three of a kind or stay in two pair
+
+  RRBB -> RRRB
+       -> RRBB  
+
+  and from three of a kind you can go to two pair, or you can go to four of a kind and victory
+
+       -> RRBB
+  RRRB -> RRRR
+       -> RRRB
+
+*/
+
+/* This is a good start.  The next step is we need to calculate the probabilities of 
+   transitioning between the different states.
+
+  high card to one pair is easy.  100% you go to one pair.
+
+  RBGY -> RRBG  1
+
+  for one pair, there is a 1/2 chance of drawing R on the first pull
+  There is a 1/3 chance of drawing another R, which gives a 1/6 chance of one pair.
+  There is a 2/3 chance of drawing a B or G, which gives a 1/3 change of three of a kind.
+
+  There is a 1/2 chance of drawing B or G on the first pull.
+  There is a 2/3 chance of then drawing R, wihch gives a 1/3 change of one pair
+  There is a 1/3 chance of instead drawing a G or B, which gives a 1/6 change of two pair
+
+  If you combine the 1/6 and 1/3 chances of getting one pair, you get.
+
+       -> RRRB 1/3
+  RRBG -> RRBB 1/6
+       -> RRBG 1/2
+
+  Similarly for the other combinations
+
+  for two pair
+
+  RRBB -> RRRB 2/3
+       -> RRBB 1/3
+
+  for three of a kind
+
+       -> RRBB 1/4 
+  RRRB -> RRRR 1/4
+       -> RRRB 1/2
+ 
 */
