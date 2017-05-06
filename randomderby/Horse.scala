711db15bad60f7a327e6b405106db8e7798c2277
Jim Stroming <jim.stroming@ibm.com>
2017-05-06 13:38:18 -0500

    fix a defect in horse

--- randomderby/Horse.scala
+++ randomderby/Horse.scala
@@ -9,13 +9,13 @@ class Horse(prob: Double) {
 
   val p = prob  // probability of going forward
   val step = 1  // horse moves either forward or backward 1
+  val cutoff = 1000 * p
 
   def move(pos: Int, rng: RNG): (Int, RNG) = {
     val (newnumber, rngnext) = rng.nextInt
-    val modnumber = newnumber % 1000
-    val cutoff = 1000 * p 
-    if (modnumber < cutoff) (step, rngnext)
-    else (-step, rngnext)
+    val modnumber = newnumber % 1000 
+    if (modnumber < cutoff) (pos + step, rngnext)
+    else (pos - step, rngnext)
   }
 
 } 
