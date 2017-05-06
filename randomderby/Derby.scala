f67075e545a62efdcd73c25be373914e7162beed
Jim Stroming <jim.stroming@ibm.com>
2017-05-06 15:03:58 -0500

    start writing the race method

--- randomderby/Derby.scala
+++ randomderby/Derby.scala
@@ -19,5 +19,19 @@ object Derby {
     }
   }
 
+  def checkforwinner(dist: List[Int], finishline: Int): List[Int] = {
+
+    dist.zipWithIndex.filter(_._1 >= finishline).map(_._2)
+
+  }
+
+
+  def race(horses: List[Horse], finishdist: Int, rng: RNG, currdist: List[Int] ): (List[Int], RNG) = {
+
+    (List(1,2,3), rng)
+
+  }
+
+
 }
 
