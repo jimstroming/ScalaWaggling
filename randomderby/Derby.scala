6a6cea242fa898adcd60001bfba186b8ec384aeb
Jim Stroming <jim.stroming@ibm.com>
2017-05-06 15:06:24 -0500

    chage the order of the parameters to put the optional parameters at the end

--- randomderby/Derby.scala
+++ randomderby/Derby.scala
@@ -8,14 +8,14 @@ import horse.Horse
 
 object Derby {
   
-  def racestep(horses: List[Horse], newdist: List[Int], olddist: List[Int], rng: RNG): (List[Int], RNG) = {
+  def racestep(horses: List[Horse], rng: RNG, olddist: List[Int], newdist: List[Int]): (List[Int], RNG) = {
 
     if (horses.length == 0) return (newdist, rng)
     else {
       val currenthorse = horses.head
       val remaininghorses = horses.tail
       val (distance, newrng) = currenthorse.move(olddist.head, rng)
-      racestep(remaininghorses, newdist:::List(distance), olddist.tail, newrng)
+      racestep(remaininghorses, newrng, olddist.tail, newdist:::List(distance))
     }
   }
 
