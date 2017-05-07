5913f17c552c3c4ea26946e8b023c9e760661981
Jim Stroming <jim.stroming@ibm.com>
2017-05-07 07:27:51 -0500

    add the problem definition for the May 5th fivethirtyeight riddler https://fivethirtyeight.com/features/who-will-win-the-lucky-derby/

--- randomderby/Derby.scala
+++ randomderby/Derby.scala
@@ -6,6 +6,28 @@ import RNG.RNG
 import simpleRNG.SimpleRNG
 import horse.Horse
 
+/* 
+
+https://fivethirtyeight.com/features/who-will-win-the-lucky-derby/
+
+The bugle sounds, and 20 horses make their way to the starting gate for the first annual Lucky Derby. 
+These horses, all trained at the mysterious Riddler Stables, are special. Each second, every 
+Riddler-trained horse takes one step. Each step is exactly one meter long. But what these horses 
+exhibit in precision, they lack in sense of direction. Most of the time, their steps are forward 
+(toward the finish line) but the rest of the time they are backward (away from the finish line). 
+As an avid fan of the Lucky Derby, you’ve done exhaustive research on these 20 competitors. 
+You know that Horse One goes forward 52 percent of the time, Horse Two 54 percent of the time, 
+Horse Three 56 percent, and so on, up to the favorite filly, Horse Twenty, who steps forward 90 
+percent of the time. The horses’ steps are taken independently of one another, and the finish 
+line is 200 meters from the starting gate.
+
+Handicap this race and place your bets! In other words, what are the odds (a percentage is fine)
+that each horse wins?
+
+*/
+
+
+
 object Derby {
   
   def racestep(horses: List[Horse], rng: RNG, olddist: List[Int], newdist: List[Int] = List()): (List[Int], RNG) = {
