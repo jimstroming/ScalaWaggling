8dc7510441387f538e65e7f54db6f1bf6328eac0
Jim Stroming <jim.stroming@ibm.com>
2017-05-07 07:24:57 -0500

    run simulations

--- randomderby/Derby.scala
+++ randomderby/Derby.scala
@@ -55,7 +55,7 @@ object Derby {
     
     if (n == 0) (win, rng)
     else {
-      println(win)
+      if (n % 1000 == 0) println(win)
       val winners = fillzeroes(horses, win)
       val (roundwinners, rng2) = race(horses, finishdist, rng)
       val newwinlist = incrementwinlist(winners, roundwinners)
@@ -66,14 +66,64 @@ object Derby {
 
   def main(args: Array[String]): Unit = {
     
-    val horses = List(new Horse(0.9), new Horse(0.9), new Horse(0.9))
+    val horses = List(new Horse(0.52), 
+                      new Horse(0.54), 
+                      new Horse(0.56),
+                      new Horse(0.58),
+                      new Horse(0.60), 
+                      new Horse(0.62), 
+                      new Horse(0.64),
+                      new Horse(0.66),
+                      new Horse(0.68), 
+                      new Horse(0.70),
+                      new Horse(0.72),
+                      new Horse(0.74), 
+                      new Horse(0.76), 
+                      new Horse(0.78),
+                      new Horse(0.80),
+                      new Horse(0.82),
+                      new Horse(0.84),
+                      new Horse(0.86)) 
+                      // new Horse(0.88))
+                      // new Horse(0.90))
+                     
+
     // val (winners, newrng) = race(horses, 2000, SimpleRNG(60))
-    val (winners, newrng) = runraces(horses, 2000, SimpleRNG(60), 100000)
+    val (winners, newrng) = runraces(horses, 2000, SimpleRNG(60), 10000000)
     println(winners)
 
   }
 
-
-
 }
+
+
+
+/* Let's run some simulations to find the answer.  
+
+List(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 536, 25863, 325467)
+List(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 539, 25931, 326406)
+List(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 539, 26002, 327344)
+List(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 543, 26083, 328271)
+List(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 548, 26166, 329198)
+List(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 549, 26229, 330140)
+List(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 551, 26289, 331084)
+List(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 551, 26362, 332017)
+
+This is taking a long time.   I have only seen horse 17, 18, 19 and 20 win to this point.
+Fortunately, we can speed this up by excluding some of the horses.
+If I eliminate horse 20, for instance, I would expect the ratio between wins of horse 18 and 19 to 
+stay the same.
+
+Let's try it out
+
+List(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 94, 3329, 33911)
+List(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 96, 3419, 34824)
+
+List(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 66, 1725, 16360)
+List(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 70, 1835, 17256)
+
+
+*/
+
+
 
